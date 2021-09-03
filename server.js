88377ce9d8f5c19b1fc42962ae167657ec065302
Esper06 <matt.walford98@gmail.com>
2021-09-03 02:00:50 +0100

    changed var to let in some functions

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express(); //we make a variable and give it the function express(). 
 const PORT = process.env.PORT || 8080; //we make a variable called port and give it the value 8080. 8080 is a popular internet port used for proxies and caching
 
 app.use(express.static('public')); //express.static will serve static files to the directory known as 'public'
-app.use(express.urlencoded({extended: true})); //this is a method that uses express to recognise incoming requests as strings or arrays
+app.use(express.urlencoded({ extended: true })); //this is a method that uses express to recognise incoming requests as strings or arrays
 app.use(express.json()); //middleware that will help recognise incoming json
 
 
@@ -25,9 +25,9 @@ app.get("/api/notes", (req, res) => {
 
 //recieves a new note from the request body, adds it to db.json, then updates the database so that the note becomes accessable
 app.post("/api/notes", (req, res) => {
-    var newNote = req.body; //a new note is made from the text in the request body and assigned to a variable
-    var noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //make a variable of all the notes parsed. This is so we can add newNote later
-    var notelength = (noteList.length).toString(); //we check how many notes we have in the list. This allows us to add id's to the notes
+    let newNote = req.body; //a new note is made from the text in the request body and assigned to a variable
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //make a variable of all the notes parsed. This is so we can add newNote later
+    let notelength = (noteList.length).toString(); //we check how many notes we have in the list. This allows us to add id's to the notes
 
     newNote.id = notelength; //the length of the note list becomes the id of the new note. E.g: if there are 5 notes and you add one more, the new note's id will be "6"
     
@@ -41,8 +41,8 @@ app.post("/api/notes", (req, res) => {
 
 //deletes a note based on what their id is. The id is added when the note is posted
 app.delete("/api/notes/:id", (req, res) => { //we target a specific note based on the id
-    var noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //once again we make a variable that is a list of all the saved notes
-    var noteId = (req.params.id).toString(); //we turn the requested id into a string. This is because the note id's are all strings
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); //once again we make a variable that is a list of all the saved notes
+    let noteId = (req.params.id).toString(); //we turn the requested id into a string. This is because the note id's are all strings
 
     noteList = noteList.filter(selected =>{ //we filter all the notes by id. The ones that do not have a matching id with the request will be saved in a new array
         return selected.id != noteId       //whereas the matching id gets deleted
